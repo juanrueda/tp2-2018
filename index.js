fda07fdfecefdfc28cebe6bcf152342bb1d753b0
Juan Rueda <juanandresrueda1@gmail.com>
2019-01-27 13:46:16 -0300

    Primer post (equipos)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const {Juego,Equipo,TipoEvento} = require('./sequelize');
-  
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
 app.get('/juegos', (req, res, next) => {
     Juego.findAll().then(juegos => res.json(juegos))
@@ -12,6 +18,15 @@ app.get('/equipos', (req, res, next) => {
 });
 app.get('/tiposEventos', (req, res, next) => {
     TipoEvento.findAll().then(tiposEventos => res.json(tiposEventos))
+});
+
+app.post('/equipos', (req, res) => {
+
+    var equipo = {
+        nombre_equipo : req.body.nombre_equipo
+    }  
+    Equipo.create(equipo);
+    res.send(equipo);
 });
 
 const port = 3000;
